a3bc419616701e6b72ce6f2de083b5fa6d72deb8
Miz <mijin709@naver.com>
2020-04-10 14:47:27 +0900

    작성일 : 2020.04.10 수정사항 : EmpMain.java

--- testproject/src/emp/EmpMain.java
+++ testproject/src/emp/EmpMain.java
@@ -1,9 +1,15 @@
 package emp;
 
 public class EmpMain {
-
+	public void show() {
+		System.out.println("안녕하세요.");
+	}
+	
 	public static void main(String[] args) {
+		EmpMain ob = new EmpMain();
+		ob.show();
 		System.out.println("Java Project");
+		
 
 	}
 
